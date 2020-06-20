484ef79ca1f85efc4ff5f270ffa8e0f9901f4edb
handlerug <handlerug@gmail.com>
2020-06-20 15:34:49 +0700

    Make Accounts pane setup a little bit faster

--- Telegram-Mac/TGAccountsPreferencesViewController.swift
+++ Telegram-Mac/TGAccountsPreferencesViewController.swift
@@ -48,16 +48,14 @@ class TGAccountsPreferencesViewController: TGPreferencesViewController, NSTableV
         tableView.dataSource = self
         
         let signal = context.sharedContext.activeAccountsWithInfo |> mapToSignal { primary, accounts -> Signal<([AccountWithInfo], [PeerId : (PeerView, CGImage?)]), NoError> in
-            let complementary = accounts.map { info -> Signal<(PeerId, PeerView, CGImage?), NoError> in
-                return combineLatest(info.account.viewTracker.peerView(info.account.peerId), peerAvatarImage(account: info.account, photo: .peer(info.peer, info.peer.smallProfileImage, info.peer.displayLetters, nil), displayDimensions: NSMakeSize(48, 48), scale: System.backingScale)) |> map { peerView, photo in
-                    (info.account.peerId, peerView, photo.0)
-                }
+            let complementary = accounts.map { info -> Signal<(PeerId, (peerView: PeerView, photo: CGImage?)), NoError> in
+                return combineLatest(info.account.viewTracker.peerView(info.account.peerId), peerAvatarImage(account: info.account, photo: .peer(info.peer, info.peer.smallProfileImage, info.peer.displayLetters, nil), displayDimensions: NSMakeSize(48, 48), scale: System.backingScale))
+                    |> map { peerView, photo in
+                        (info.account.peerId, (peerView: peerView, photo: photo.0))
+                    }
             }
             
             return combineLatest(complementary) |> map { complementary in
-                let complementary = complementary.map {
-                    return ($0.0, (peerView: $0.1, photo: $0.2))
-                }
                 let dict: [PeerId: (PeerView, CGImage?)] = complementary.reduce([:], { result, current in
                     var result = result
                     result[current.0] = current.1
